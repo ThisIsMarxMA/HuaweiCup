e4bdb9a3af712fac326dd1678ad6992845e64db7
ThisIsMarxMA <ma376371893@163.com>
2020-09-17 21:37:06 +0800

    完善第一问程序

--- B题/Q1_step1.m
+++ B题/Q1_step1.m
@@ -1,12 +1,12 @@
-%将空值处使用前后两小时平均数据代替 如果空值个数较少
-function[]=Q1_step1(data,ratio) %ratio取0.5
+%将空值处使用前后两小时平均数据代替 如果空值个数较多，则整列置0
+function[data]=Q1_step1(data,ratio) %ratio取0.5
     [n,dim]=size(data);
     for i=1:dim     %遍历位点
         vec=data(:,i);
-        len=n;
+        len=length(vec);
         k=1;
         num=zeros(40,1);
-        for j=1:n
+        for j=1:length(vec)
             if vec(j)==0   %如果该处值为0
                 len=len-1;
                 num(k)=j;   %记录0位置点
@@ -14,11 +14,13 @@ function[]=Q1_step1(data,ratio) %ratio取0.5
             end
         end
         %如果存在为0的地方且不是太多，则用均值覆盖0
-        if len >= round(n*ratio) && len < length(vec)
+        if len >= round(length(vec)*ratio) && len < length(vec)
             meanVec=sum(vec)/len;
             for j=1:k-1
                 data(num(j),i)=meanVec;
             end
+        elseif len < round(length(vec)*ratio)
+            data(:,i)=0;    %如果0太多就全置0
         end
     end
 end
--- B题/Q1_step2.m
+++ B题/Q1_step2.m
@@ -1,15 +1,31 @@
 % 354个操作变量的取值范围与delta误差
-function[]=Q1_step2(data,range) 
-    for i=1:size(range)
-        line=range(i);
-        range(i)=regexp(line,'-','split');
+function[data]=Q1_step2(data,fanwei) 
+    b=zeros(length(fanwei),2);
+    for i=1:size(fanwei)
+        aaa=fanwei(i);
+        temp=cell2mat(regexp(aaa{1,1},'[^（^）^(^)]','match'));   %去除括号
+        L1=length(temp);    %长度一
+        
+        temp2=regexp(temp,'-','split');
+        L2=sum(cellfun('length',temp2));  %长度二
+        
+        if L1-L2==1
+            b(i,1)=str2double(temp2(1));
+            b(i,2)=str2double(temp2(2));
+        elseif L1-L2==2
+            b(i,1)=-str2double(temp2(2));
+            b(i,2)=str2double(temp2(3));
+        elseif L1-L2==3
+            b(i,1)=-str2double(temp2(2));
+            b(i,2)=-str2double(temp2(4));
+        end
     end
     
     [n,dim]=size(data);
     for i=1:n %对40个行向量取值
         for j=1:dim
-            if data(i,j)<range{1,j}(1) || dataNo313(i,j)>range{1,j}(2) %如果有不符合范围的
-                data(i,j)=Inf; 
+            if data(i,j)<b(j,1) || data(i,j)>b(j,2) %如果有不符合范围的
+                data(i,j)=0;    %超出范围则置零 
             end
         end
     end
